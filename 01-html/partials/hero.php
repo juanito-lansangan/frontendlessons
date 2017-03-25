871e30debe1fa1778bf202c74ce7b1f34c6faed4
Juanito Lansangan <juanito.lansangan@codeandtheory.com>
2017-03-25 12:13:21 +0800

    03/25/17 complete styles for sticky navigation.

--- 01-html/partials/hero.php
+++ 01-html/partials/hero.php
@@ -6,6 +6,6 @@
 				<img src="https://lh6.googleusercontent.com/-wL668xtWx0I/AAAAAAAAAAI/AAAAAAAAAAA/5iH4rLa5Gok/s128-c-k/photo.jpg" alt="image text">
 			</picture>
 		</figure>
-		<h1 class="c-hero__title"><a href="#">JPlans</a></h1>
-	</div>
+	</div>	
+	<h1 class="c-hero__title"><a href="#">JPlans</a></h1>
 </header>
--- 01-html/partials/navigation.php
+++ 01-html/partials/navigation.php
@@ -1,7 +1,15 @@
 <nav class="c-nav">
-	<div class="o-container">
-		<img class="c-nav__logo" src="https://lh6.googleusercontent.com/-wL668xtWx0I/AAAAAAAAAAI/AAAAAAAAAAA/5iH4rLa5Gok/s128-c-k/photo.jpg" alt="image text">
-		<h6 class="c-nav__title"><a href="#">Jplans</a></h6>
-		<div class="c-nav__burger"><span></span></div>
+	<div class="c-nav__container container">
+		<h6 class="c-nav__title">
+			<a href="#">
+				<img class="c-nav__logo" src="https://lh6.googleusercontent.com/-wL668xtWx0I/AAAAAAAAAAI/AAAAAAAAAAA/5iH4rLa5Gok/s128-c-k/photo.jpg" alt="image text">
+				Jplans
+			</a>
+		</h6>
+		<div class="o-burger-nav">
+			<div class="o-burger-nav__icon">
+				<span></span>
+			</div>
+		</div>
 	</div>
 </nav>
